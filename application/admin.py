27fff886647165cdd79ce40faddfd50a263638c3
skantor <serhiykantor@gmail.com>
2023-02-24 11:50:20 +0200

    register models

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from application.models import User, Message, Thread
+
+admin.site.register(User)
+admin.site.register(Message)
+admin.site.register(Thread)
